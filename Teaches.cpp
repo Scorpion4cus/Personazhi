f45e3444e64e35c65994da03a3ce5e9e28a4714f
Vladislav <104004655+Scorpion4cus@users.noreply.github.com>
2022-10-03 14:02:49 +0700

    Update and rename windranger.cpp to Teaches.cpp

---
+++ Teaches.cpp
@@ -0,0 +1,9 @@
+#include "Enigma.h"
+
+Enigma::Enigma()
+{
+    this->health=110;
+    this->armor=13;
+    this->damage=221;
+        strcpy(this->name, "AAA");
+}
--- windranger.cpp
+++
@@ -1,9 +0,0 @@
-#include "windranger.h"
-
-WindRanger::WindRanger()
-{
-    this->health=10;
-    this->armor=111;
-    this->damage=122;
-        strcpy(this->name, "WWW");
-}
